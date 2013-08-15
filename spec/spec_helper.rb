28a392c3e315cc157f1d1d6d38431cb6317c33c2
Vladimir Moravec <vmoravec@suse.com>
2013-08-15 15:47:34 +0200

    Add generated spec dir with spec file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'yast'
+
+# Use `require_relative "spec_helper"` on top of your spec files to be able to
+# run them separately with command `ruby spec/some_spec.rb`
+# Use `rake spec` to run the whole testsuite.
