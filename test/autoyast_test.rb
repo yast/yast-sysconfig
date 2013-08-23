ea3f479f3a912a9c2e15e418ecd07951c2b1e6c8
Vladimir Moravec <vmoravec@suse.com>
2013-08-23 16:21:53 +0200

    Improve autoyast tests

--- test/autoyast_test.rb
+++ test/autoyast_test.rb
@@ -1,61 +1,120 @@
 require_relative 'test_helper'
 
-include TestHelpers
+describe "Autoyast configuration" do
 
-describe "Import and export of autoyast configuration" do
-  before do
-    initialize_sysconfig
-  end
+  describe "How the profile import works" do
 
-  it "can import correctly defined configuration values" do
-    load_sample :network_config do
-      var_name  = 'FIREWALL'
-      var_value = 'no'
-      profile_values = [{
-        'sysconfig_key'   => var_name,
-        'sysconfig_value' => var_value,
-        'sysconfig_path'  => sample_path
-      }]
-      sysconfig.Import(profile_values)
-      sysconfig.Modified.must_equal true
-      sysconfig.Export.wont_be_empty
+    include TestHelpers
+
+    before do
+      initialize_sysconfig
+    end
+
+    it "succeeds with correctly defined values" do
+      load_sample :network_config do
+        var_name  = 'FIREWALL'
+        var_value = 'no'
+        profile_values = [{
+          'sysconfig_key'   => var_name,
+          'sysconfig_value' => var_value,
+          'sysconfig_path'  => sample_path
+        }]
+        sysconfig.Import(profile_values)
+        sysconfig.Modified.must_equal true
+      end
+    end
+
+    it "succeeds with incorrectly defined values" do
+      load_sample :network_config do
+        var_name  = 'FIREBALL'
+        var_value = 'DONT TOUCH THIS'
+        profile_values = [{
+          'sysconfig_key'   => var_name,
+          'sysconfig_value' => var_value,
+          'sysconfig_path'  => sample_path
+        }]
+        sysconfig.Import(profile_values)
+        sysconfig.Modified.must_equal true
+      end
+    end
+
+    it "succeeds even with totally random defined variable names and values" do
+      load_sample :network_config do
+        profile_values = [{
+          'not_expected_key'   => rand(88888),
+          'not_expected_value' => rand(88888),
+          'not_expected_path'  => rand(88888)
+        }]
+        sysconfig.Import(profile_values).must_equal true
+        sysconfig.Modified.must_equal true
+      end
     end
   end
 
-  it "can import incorrectly defined config values" do
-    load_sample :network_config do
-      var_name  = 'FIREBALL'
-      var_value = 'DONT TOUCH THIS'
-      profile_values = [{
-        'sysconfig_key'   => var_name,
-        'sysconfig_value' => var_value,
-        'sysconfig_path'  => sample_path
-      }]
-      sysconfig.Import(profile_values)
-      sysconfig.Modified.must_equal true
-      sysconfig.Export.wont_be_empty
-      sysconfig.Export.first['sysconfig_key'].wont_be_empty
-      sysconfig.Export.first['sysconfig_key'].must_equal(var_name)
-      sysconfig.Export.first['sysconfig_value'].wont_be_empty
-      sysconfig.Export.first['sysconfig_value'].must_equal(var_value)
-      sysconfig.Export.first['sysconfig_path'].wont_be_empty
-      sysconfig.Export.first['sysconfig_path'].must_equal(sample_path)
+  describe "When exporting autoyast profile" do
+    before do
+      initialize_sysconfig
     end
-  end
 
-  it "accepts totally random defined variable names and values" do
-    load_sample :network_config do
-      profile_values = [{
-        'not_expected_key'   => rand(88888),
-        'not_expected_value' => rand(88888),
-        'not_expected_path'  => rand(88888)
-      }]
-      sysconfig.Import(profile_values).must_equal true
-      sysconfig.Modified.must_equal true
-      sysconfig.Export.wont_be_empty
-      sysconfig.Export.first['sysconfig_key'].must_be_empty
-      sysconfig.Export.first['sysconfig_value'].must_be_empty
-      sysconfig.Export.first['sysconfig_path'].wont_be_empty # default value is set
+    it "succeeds with correctly defined values" do
+      load_sample :network_config do
+        var_name  = 'NOZEROCONF'
+        var_value = 'yes'
+        profile_values = [{
+          'sysconfig_key'   => var_name,
+          'sysconfig_value' => var_value,
+          'sysconfig_path'  => sample_path
+        }]
+        sysconfig.Import(profile_values)
+        sysconfig.Modified.must_equal true
+        sysconfig.Export.wont_be_empty
+        sysconfig.Export.first['sysconfig_key'].wont_be_empty
+        sysconfig.Export.first['sysconfig_value'].wont_be_empty
+        sysconfig.Export.first['sysconfig_path'].wont_be_empty
+        sysconfig.Export.first['sysconfig_path'].must_equal(sample_path)
+        sysconfig.Export.first['sysconfig_key'].must_equal(var_name)
+        sysconfig.Export.first['sysconfig_value'].must_equal(var_value)
+      end
+    end
+
+    it "succeeds with incorrectly defined values" do
+      load_sample :network_config do
+        var_name  = 'DEBUG'
+        var_value = 'NOWAYTHISCORRECTVALUE'
+        profile_values = [{
+          'sysconfig_key'   => var_name,
+          'sysconfig_value' => var_value,
+          'sysconfig_path'  => sample_path
+        }]
+        sysconfig.Import(profile_values)
+        sysconfig.Modified.must_equal true
+        sysconfig.Export.wont_be_empty
+        sysconfig.Export.first['sysconfig_key'].wont_be_empty
+        sysconfig.Export.first['sysconfig_key'].must_equal(var_name)
+        sysconfig.Export.first['sysconfig_value'].wont_be_empty
+        sysconfig.Export.first['sysconfig_value'].must_equal(var_value)
+        sysconfig.Export.first['sysconfig_path'].wont_be_empty
+        sysconfig.Export.first['sysconfig_path'].must_equal(sample_path)
+      end
+    end
+
+    it "succeeds even with totally random defined variable names and values" do
+      load_sample :network_config do
+        profile_values = [{
+          'not_expected_key'   => rand(88888),
+          'not_expected_value' => rand(88888),
+          'not_expected_path'  => rand(88888)
+        }]
+        sysconfig.Import(profile_values).must_equal true
+        sysconfig.Modified.must_equal true
+        sysconfig.Export.wont_be_empty
+        sysconfig.Export.first['sysconfig_key'].must_be_empty
+        sysconfig.Export.first['not_expected_key'].must_be_nil
+        sysconfig.Export.first['sysconfig_value'].must_be_empty
+        sysconfig.Export.first['not_expected_value'].must_be_nil
+        sysconfig.Export.first['sysconfig_path'].wont_be_empty # default value is set
+        sysconfig.Export.first['not_expected_path'].must_be_nil
+      end
     end
   end
 end
